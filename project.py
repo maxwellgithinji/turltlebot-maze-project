17961aa1216bb30363c0f9a10dbce8e7fbd167d9
Charles Maxwel Githinji Waweru <maxwellgithinji@gmail.com>
2024-11-24 23:53:35 +0000

    Make a turn when you get a barrier

--- project.py
+++ project.py
@@ -6,7 +6,6 @@ class Maze(RobotControl):
     def __init__(self, route=1):
         super().__init__()
         self.route = route
-        self.front_barrier_proximity = 1
         self.front_laser = self.get_front_laser()
         self.front_left_laser = self.get_laser(240)
         self.front_right_laser = self.get_laser(480)
@@ -22,6 +21,9 @@ class Maze(RobotControl):
         self.set_front_lasers()
         rospy.loginfo("Stopping robot, barrier %fm away", self.front_laser)
 
+        # try to turn the robot after stopping
+        self.make_turn()
+
     def set_front_lasers(self):
         full_laser = self.get_laser_full()
         self.front_laser =  full_laser[360]
@@ -29,9 +31,34 @@ class Maze(RobotControl):
         self.front_right_laser = full_laser[480]
     
     def can_move_staright(self):
-        return self.front_laser >self.front_barrier_proximity and self.front_left_laser > self.front_barrier_proximity and self.front_right_laser > self.front_barrier_proximity
+        front_barrier_proximity = 1
+        return self.front_laser > front_barrier_proximity and self.front_left_laser > front_barrier_proximity and self.front_right_laser > front_barrier_proximity
 
+    def make_turn(self):
+        if self.should_turn_left():
+            rospy.loginfo("turning left, front left laser proximity, %fm", self.front_left_laser)
+            initial_reading = self.front_left_laser
+            while not self.left_oversteer(initial_reading):
+                self.rotate(-10)
+                self.set_front_lasers()
+        elif self.should_turn_right():
+            rospy.loginfo("turning right, front right laser proximity %fm", self.front_right_laser)
+            initial_reading = self.front_right_laser 
+            while not self.right_oversteer(initial_reading):
+                self.rotate(10)
+                self.set_front_lasers()
 
+    def should_turn_left(self):
+        return self.front_left_laser > self.front_laser
+    def should_turn_right(self):
+        return self.front_right_laser > self.front_laser
+    
+    def left_oversteer(self, initial_reading):
+        return self.front_left_laser < initial_reading
+        
+    def right_oversteer(self, initial_reading):
+        return self.front_right_laser < initial_reading
+    
 m = Maze()
 
 m.navigate()
